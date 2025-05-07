b437152407f29b58470d93d2bdd10797bab5e030
Tetiana Klitna <klitna.tetiana@gmail.com>
2025-05-06 20:21:49 -0700

    added missed import

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Login from './components/Authentication/Login';
-import Signup from './components/Authentication/SignUp';
+import SignUp from './components/Authentication/SignUp';
 import Forgot from './components/Authentication/ForgotPassword';
+import ResetPassword from './components/Authentication/ResetPassword';
 import Navbar from './components/main/Navbar';
 import Footer from './components/main/Footer';
 
@@ -29,13 +30,13 @@ function App() {
             <Route element={<ProtectedRoutes />} />
             <Route path="about" element={<About />} />
             <Route path="book-now" element={<BookNow />} />
-            <Route path="signup" element={<Signup />} />
+            <Route path="signup" element={<SignUp />} />
             <Route path="calendar" element={<BookingCalendarPage />} />
             <Route path="hotels" element={<HotelsPage />} />
             <Route path="hotels/:hotelId" element={<HotelDetailsPage />} />
             <Route path="login" element={<Login />} />
             <Route path="forgotpassword" element={<Forgot />} />
-              <Route path="reset-password" element={<ResetPassword />} />
+            <Route path="reset-password" element={<ResetPassword />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
