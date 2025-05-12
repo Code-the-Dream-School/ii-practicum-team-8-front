8dddfd15f72944e4db6e9de9b2cfdb000381cc6e
Tetiana Klitna <klitna.tetiana@gmail.com>
2025-05-11 18:47:25 -0700

    added validation in getAuthHeaders

--- src/util/apiCalendar.js
+++ src/util/apiCalendar.js
@@ -1,9 +1,14 @@
 import { getAllData, deleteData, postData, updateData } from './index';
 const endpointCalendar = `${import.meta.env.VITE_APP_API_URL}/api/v1/bookings`;
-const getAuthHeaders = (token) => ({
-  Authorization: `Bearer ${token}`,
-  'Content-Type': 'application/json',
-});
+const getAuthHeaders = (token) => {
+  if (!token) {
+    throw new Error('No authentication token found. User is not authorized.');
+  }
+  return {
+    Authorization: `Bearer ${token}`,
+    'Content-Type': 'application/json',
+  };
+};
 
 const getAllBookings = async (token) => {
   const res = await getAllData(endpointCalendar, {}, getAuthHeaders(token));
@@ -14,19 +19,33 @@ const getAllBookings = async (token) => {
 };
 
 const deleteBooking = async (bookingId, token) => {
-  const res = await deleteData(`${endpointCalendar}/${bookingId}`, {}, getAuthHeaders(token));
-  return res||{};
+  const res = await deleteData(
+    `${endpointCalendar}/${bookingId}`,
+    {},
+    getAuthHeaders(token)
+  );
+  return res || {};
 };
 
 const updateBooking = async (booking, token) => {
   const { bookingId, ...requestBody } = booking;
-  const res = await updateData(`${endpointCalendar}/${bookingId}`, requestBody, {}, getAuthHeaders(token));
-  return res||{};
+  const res = await updateData(
+    `${endpointCalendar}/${bookingId}`,
+    requestBody,
+    {},
+    getAuthHeaders(token)
+  );
+  return res || {};
 };
 
 const createBooking = async (booking, token) => {
-  const res = await postData(endpointCalendar, booking, {}, getAuthHeaders(token));
-  return res||{};
+  const res = await postData(
+    endpointCalendar,
+    booking,
+    {},
+    getAuthHeaders(token)
+  );
+  return res || {};
 };
 
 export { getAllBookings, deleteBooking, updateBooking, createBooking };
